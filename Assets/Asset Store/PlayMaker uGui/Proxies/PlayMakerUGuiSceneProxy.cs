fd60e9414c408c6861a6deb79fb4fd64cf028b76
Tomas Trescak <tomi.trescak@gmail.com>
2018-10-09 09:30:31 +1100

    Initial Commit

---
+++ Assets/Asset Store/PlayMaker uGui/Proxies/PlayMakerUGuiSceneProxy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using HutongGames.PlayMaker;
+
+public class PlayMakerUGuiSceneProxy : MonoBehaviour {
+
+
+	public static PlayMakerFSM fsm;
+
+	// Use this for initialization
+	void Start () {
+
+		if (fsm != null)
+		{
+			Destroy (this.gameObject);
+			return;
+		}
+
+		PlayMakerUGuiSceneProxy.fsm = GetComponent<PlayMakerFSM>();
+	}
+
+}
